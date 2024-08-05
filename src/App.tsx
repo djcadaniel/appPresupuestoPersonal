ffe6ae2394bf3a2af39f9901a2156b51d0f1633e
djcadaniel <djcadaniel@gmail.com>
2024-08-05 12:59:10 -0500

    form end

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ function App() {
 
   return (
     <>
-      <div className="w-full max-w-7xl mx-auto p-3 mb-10 md:mb-20 text-white bg-[#2B2B3B] flex justify-center md:justify-start items-center gap-5 ">
+      <div className="w-full max-w-7xl mx-auto p-3 mb-6 md:mb-20 text-white bg-[#2B2B3B] flex justify-center md:justify-start items-center gap-5 ">
         <img src={logo} alt="logo de presupuesto" className="h-12 md:h-16"/>
         <h1 className="text-xl md:text-3xl font-changa">Presupuesto personal</h1>
       </div>
-      <PresupuestoForm />
+      <div className="p-5">
+        <PresupuestoForm />
+      </div>
     </>
   )
 }
--- src/components/PresupuestoForm.tsx
+++ src/components/PresupuestoForm.tsx
@@ -1,22 +1,40 @@
 
 export default function PresupuestoForm() {
   return (
-    <form className="bg-[#2B2B3B]">
-      <div className="flex flex-col space-y-5">
-        <label 
-          htmlFor="presupest"
-          className="text-[1rem] md:text-[1.2rem] text-[#757569] text-center"
-        >
-          Ingresa presupuesto:
-        </label>
-        <input 
-          id='presupuest'
-          type="number"
-          value='Presupuesto' 
-        />
+    <form className="w-full md:w-1/2 mx-auto">
+      <div className="bg-[#2B2B3B] rounded-md">
+        <div className="flex flex-col items-start space-y-5 p-5">
+          <label 
+            htmlFor="name"
+            className="text-[1rem] md:text-[1.2rem] text-[#757569] text-center"
+          >
+            Nombre:
+          </label>
+          <input 
+            id='name'
+            type="number"
+            placeholder='Ingresa tus nombres'
+            className="w-full p-2 rounded-sm bg-[#3d3d52] text-white placeholder-[#6f6d68] border-b-2 border-white outline-none focus:outline focus:outline-[#279199] focus:border-b-0"
+          />
+        </div>
+        <div className="flex flex-col items-start space-y-5 p-5">
+          <label 
+            htmlFor="presupuest"
+            className="text-[1rem] md:text-[1.2rem] text-[#757569] text-center"
+          >
+            Presupuesto:
+          </label>
+          <input 
+            id='presupuest'
+            type="number"
+            value='Presupuesto'
+            className="w-full p-2 rounded-sm bg-[#3d3d52] text-white placeholder-[#6f6d68] border-b-2 border-white outline-none focus:outline focus:outline-[#279199] focus:border-b-0"
+          />
+        </div>
       </div>
       <input 
         type="submit"
+        className="w-full p-2 bg-gradient-to-r from-[#572EF1] to-[#249794] mt-10 text-white "
       />
     </form>
   )
